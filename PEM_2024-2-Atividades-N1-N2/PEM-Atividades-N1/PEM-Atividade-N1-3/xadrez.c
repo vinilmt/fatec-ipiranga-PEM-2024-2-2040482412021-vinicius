4fa790fe3c892789385f6be82d3585ed6296fd8f
vinilmt <viniciuslmtcontato@gmail.com>
2024-08-28 23:13:23 -0300

    ansi output

--- PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-3/xadrez.c
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-3/xadrez.c
@@ -20,13 +20,14 @@ typedef enum {
     PP1, PP2, PP3, PP4, PP5, PP6, PP7, PP8, // Peões pretos
     TOTAL_PIECE_KINDS
 } PIECE;
-const char* PIECE_DISPLAY[TOTAL_PIECE_KINDS] = {
-    " X ",
-    "Bt1", "Bc2", "Bb3", "Bd ", "Br ", "Bb6", "Bc7", "Bt8",
-    "Bp1", "Bp2", "Bp3", "Bp4", "Bp5", "Bp6", "Bp7", "Bp8",
-    "Pt1", "Pc2", "Pb3", "Pd ", "Pr ", "Pb6", "Pc7", "Pt8",
-    "Pp1", "Pp2", "Pp3", "Pp4", "Pp5", "Pp6", "Pp7", "Pp8",
+const char* PIECE_DISPLAY_ANSI[TOTAL_PIECE_KINDS] = {
+    "\033[1;36m X ",
+    "\033[1;97mBt1", "\033[1;97mBc2", "\033[1;97mBb3", "\033[1;97mBd ", "\033[1;97mBr ", "\033[1;97mBb6", "\033[1;97mBc7", "\033[1;97mBt8",
+    "\033[1;97mBp1", "\033[1;97mBp2", "\033[1;97mBp3", "\033[1;97mBp4", "\033[1;97mBp5", "\033[1;97mBp6", "\033[1;97mBp7", "\033[1;97mBp8",
+    "\033[1;38;5;16mPt1", "\033[1;38;5;16mPc2", "\033[1;38;5;16mPb3", "\033[1;38;5;16mPd ", "\033[1;38;5;16mPr ", "\033[1;38;5;16mPb6", "\033[1;38;5;16mPc7", "\033[1;38;5;16mPt8",
+    "\033[1;38;5;16mPp1", "\033[1;38;5;16mPp2", "\033[1;38;5;16mPp3", "\033[1;38;5;16mPp4", "\033[1;38;5;16mPp5", "\033[1;38;5;16mPp6", "\033[1;38;5;16mPp7", "\033[1;38;5;16mPp8",
 };
+ 
  
 int main() {
     PIECE board[DIM * DIM] = {
@@ -42,13 +43,37 @@ int main() {
  
     printf("   .................................................\n");
     for (int i = 0; i < DIM; i++) {
-        printf(" %d |", i + 1); 
-        for (int j = 0; j < DIM; j++) {
-            printf(" %s |", PIECE_DISPLAY[board[i * DIM + j]]);
+        int j;
+
+        printf(" %d |", i + 1);
+        for (j = 0; j < DIM; j++) {
+            printf("\033[48;5;247m %s \033[0m|", PIECE_DISPLAY_ANSI[board[i * DIM + j]]);
+            j++;
+            printf("\033[48;5;61m %s \033[0m|", PIECE_DISPLAY_ANSI[board[i * DIM + j]]);
         }
-        printf("\n");
-        printf("   |     |     |     |     |     |     |     |     |\n");
-        printf("   |.....|.....|.....|.....|.....|.....|.....|.....|\n");
+        printf("\n   |");
+        for (j = 0; j < DIM; j++) {
+            printf("\033[1;48;5;247m     \033[0m|");
+            j++;
+            printf("\033[1;48;5;61m     \033[0m|");
+        }
+        printf("\n   |.....|.....|.....|.....|.....|.....|.....|.....|\n");
+
+        i++;
+
+        printf(" %d |", i + 1);
+        for (j = 0; j < DIM; j++) {
+            printf("\033[1;48;5;61m %s \033[0m|", PIECE_DISPLAY_ANSI[board[i * DIM + j]]);
+            j++;
+            printf("\033[1;48;5;247m %s \033[0m|", PIECE_DISPLAY_ANSI[board[i * DIM + j]]);
+        }
+        printf("\n   |");
+        for (j = 0; j < DIM; j++) {
+            printf("\033[1;48;5;61m     \033[0m|");
+            j++;
+            printf("\033[1;48;5;247m     \033[0m|");
+        }
+        printf("\n   |.....|.....|.....|.....|.....|.....|.....|.....|\n");
     }
     printf("      A     B     C     D     E     F     G     H\n");
     
