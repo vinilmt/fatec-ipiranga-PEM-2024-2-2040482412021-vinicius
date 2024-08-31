ff14fbf785a230924cf6abb225365b893ebb9453
vinilmt <viniciuslmtcontato@gmail.com>
2024-08-30 22:47:36 -0300

    ajuste nos nomes das peças

--- PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-3/xadrez.c
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-3/xadrez.c
@@ -22,9 +22,9 @@
 // Cada estado do enum está associado a um valor númerico, a partir de 0
 typedef enum {
     X, // Vazio
-    BT1, BC2, BB3, BD , BR , BB6, BC7, BT8, // Peças brancas
+    BT1, BC1, BB1, BD1, BR1, BB2, BC2, BT2, // Peças brancas
     BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8, // Peões brancos
-    PT1, PC2, PB3, PD , PR , PB6, PC7, PT8, // Peças pretas
+    PT1, PC1, PB1, PD1, PR1, PB2, PC2, PT2, // Peças pretas
     PP1, PP2, PP3, PP4, PP5, PP6, PP7, PP8, // Peões pretos
     TOTAL_PIECE_KINDS // Esse estado extra corresponde ao total de estados (exceto este)
 } PIECE;
@@ -32,22 +32,22 @@ typedef enum {
 // Representação ANSI de cada peça, acessada através de índice (o valor associado aos estados do enum)
 const char* PIECE_DISPLAY_ANSI[TOTAL_PIECE_KINDS] = {
     ESC NEUTRAL " X ",
-    ESC WHITE "Bt1", ESC WHITE "Bc2", ESC WHITE "Bb3", ESC WHITE "Bd ", ESC WHITE "Br ", ESC WHITE "Bb6", ESC WHITE "Bc7", ESC WHITE "Bt8",
+    ESC WHITE "Bt1", ESC WHITE "Bc1", ESC WHITE "Bb1", ESC WHITE "Bd1", ESC WHITE "Br1", ESC WHITE "Bb2", ESC WHITE "Bc2", ESC WHITE "Bt2",
     ESC WHITE "Bp1", ESC WHITE "Bp2", ESC WHITE "Bp3", ESC WHITE "Bp4", ESC WHITE "Bp5", ESC WHITE "Bp6", ESC WHITE "Bp7", ESC WHITE "Bp8",
-    ESC BLACK "Pt1", ESC BLACK "Pc2", ESC BLACK "Pb3", ESC BLACK "Pd ", ESC BLACK "Pr ", ESC BLACK "Pb6", ESC BLACK "Pc7", ESC BLACK "Pt8",
+    ESC BLACK "Pt1", ESC BLACK "Pc1", ESC BLACK "Pb1", ESC BLACK "Pd1", ESC BLACK "Pr1", ESC BLACK "Pb2", ESC BLACK "Pc2", ESC BLACK "Pt2",
     ESC BLACK "Pp1", ESC BLACK "Pp2", ESC BLACK "Pp3", ESC BLACK "Pp4", ESC BLACK "Pp5", ESC BLACK "Pp6", ESC BLACK "Pp7", ESC BLACK "Pp8",
 };
  
 int main() {
     PIECE board[DIM * DIM] = {
-        PT1, PC2, PB3, PD , PR , PB6, PC7, PT8,
+        PT1, PC1, PB1, PD1, PR1, PB2, PC2, PT2,
         PP1, PP2, PP3, PP4, PP5, PP6, PP7, PP8,
          X ,  X ,  X ,  X ,  X ,  X ,  X ,  X ,
          X ,  X ,  X ,  X ,  X ,  X ,  X ,  X ,
          X ,  X ,  X ,  X ,  X ,  X ,  X ,  X ,
          X ,  X ,  X ,  X ,  X ,  X ,  X ,  X ,
         BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8,
-        BT1, BC2, BB3, BD , BR , BB6, BC7, BT8,
+        BT1, BC1, BB1, BD1, BR1, BB2, BC2, BT2,
     };
  
     char* bg_1;
@@ -64,7 +64,7 @@ int main() {
             bg_2 = BACKGROUND_LIGHT;
         }
     
-        printf(" %d |", i + 1);
+        printf(" %d |", 8 - i);
         for (int j = 0; j < DIM; j++) {
             char* bg = (j % 2 == 0) ? bg_1 : bg_2; // Alterna a cor de fundo a cada espaço
             printf(ESC "%s %s " ESC RESET "|", bg, PIECE_DISPLAY_ANSI[board[i * DIM + j]]);
