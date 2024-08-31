2193ec2d345bca99f63149952bfe5995c07e78b3
vinilmt <viniciuslmtcontato@gmail.com>
2024-08-31 05:22:43 -0300

    declaração adiantada das funções

--- PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-4/xeque-pastor.c
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-4/xeque-pastor.c
@@ -14,7 +14,6 @@
 #define ESC "\033" // ANSI Escape 
 #define WHITE "[1;97m" // Cor para peças brancas
 #define BLACK "[1;38;5;16m" // Cor para peças pretas
-#define NEUTRAL "[1;33m" // Cor para espaço vazio
 #define BACKGROUND_LIGHT "[48;5;163m" // Cor de fundo primária
 #define BACKGROUND_DARK "[48;5;57m" // Cor de fundo secundária
 #define RESET "[0m" // Resetar todos os atributos ANSI para o padrão
