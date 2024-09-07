14f7b1db99b8851a82f88b15aa69db231a7c58f6
vinilmt <viniciuslmtcontato@gmail.com>
2024-09-06 23:35:27 -0300

    refatoração completa com matriz

--- PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-4/xeque-pastor.c
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-4/xeque-pastor.c
@@ -2,12 +2,13 @@
  * Disciplina: Programaçao Estruturada e Modular               *
  *          Prof. Carlos Veríssimo                             *
  *-------------------------------------------------------------*
- * Objetivo do Programa: Implementação da Jogada Xeque Pastor  *
+ * Objetivo do Programa: Implementação da Jogada xeque Pastor  *
  * Data - 06/09/2024                                           *
  * Autor: Vinicius Leon Melo Tavares                           *
  *-------------------------------------------------------------*/
  
 #include <stdio.h>
+#include <stdbool.h>
 
 #define DIM 8 // Dimensão do tabuleiro
 
@@ -20,21 +21,19 @@
 
 // Cada estado do enum está associado a um valor númerico, a partir de 0
 typedef enum {
-    X, // Vazio
-    WHITE_TRAIL, // Rastro de peça branca
-    BLACK_TRAIL, // Rastro de peça preta
-    BT1, BC1, BB1, BD1, BR1, BB2, BC2, BT2, // Peças brancas
-    BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8, // Peões brancos
-    PT1, PC1, PB1, PD1, PR1, PB2, PC2, PT2, // Peças pretas
-    PP1, PP2, PP3, PP4, PP5, PP6, PP7, PP8, // Peões pretos
-    TOTAL_PIECE_KINDS // Esse estado extra corresponde ao total de estados (exceto este)
-} PIECE;
-typedef enum { OK, ERR } STATUS; // A função retorna isso quando pode falhar
-
-typedef struct { int value; STATUS status; } ResultInt; // Quando retorna um valor inteiro mas pode falhar
+    Empty, // Vazio
+    WhiteTrail, // Rastro de peça branca
+    BlackTrail, // Rastro de peça preta
+    Bt1, Bc1, Bb1, Bd1, Br1, Bb2, Bc2, Bt2, // Peças brancas
+    Bp1, Bp2, Bp3, Bp4, Bp5, Bp6, Bp7, Bp8, // Peões brancos
+    Pt1, Pc1, Pb1, Pd1, Pr1, Pb2, Pc2, Pt2, // Peças pretas
+    Pp1, Pp2, Pp3, Pp4, Pp5, Pp6, Pp7, Pp8, // Peões pretos
+} Slot;
+typedef enum { Ok, Err } Status; // A função retorna isso quando pode falhar
+typedef enum { White, Black } Color; // Irá alternar entre um e outro durante o jogo para comportamentos específicos
 
 // Representação ANSI de cada peça, acessada através de índice (o valor associado aos estados do enum)
-const char* PIECE_DISPLAY_ANSI[TOTAL_PIECE_KINDS] = {
+const char* SLOT_DISPLAY_ANSI[] = {
     "   ",
     WHITE "...",
     BLACK "...",
@@ -44,152 +43,142 @@ const char* PIECE_DISPLAY_ANSI[TOTAL_PIECE_KINDS] = {
     BLACK "Pp1", BLACK "Pp2", BLACK "Pp3", BLACK "Pp4", BLACK "Pp5", BLACK "Pp6", BLACK "Pp7", BLACK "Pp8",
 };
 
-void displayBoard(PIECE* board);
-ResultInt convertCoordinatesToIndex(char* coord);
-void whiteMoveTo(PIECE* board, int from, int to);
-void blackMoveTo(PIECE* board, int from, int to);
-void clearPosition(PIECE* board, int index);
+void printBoard(Slot board[][DIM]) {
+    char* backgroundPattern[2] = {BACKGROUND_LIGHT, BACKGROUND_DARK};
+    int patternIndex = 0;
+
+    for (int i = 0; i < DIM; i++) {
+        printf(NEUTRAL " %d " RESET, 8 - i);
+
+        for (int j = 0; j < DIM; j++) {
+            char* bg = backgroundPattern[patternIndex];
+            patternIndex = 1 - patternIndex; // Alterna a cor de fundo a cada espaço
+            printf("%s %s " RESET, bg, SLOT_DISPLAY_ANSI[board[i][j]]);
+        }
+        printf("\n   ");
+
+        // Gera a área vazia em baixo de cada espaço
+        for (int j = 0; j < DIM; j++) {
+            char* bg = backgroundPattern[patternIndex];
+            patternIndex = 1 - patternIndex; // Alterna a cor de fundo a cada espaço
+            printf("%s     " RESET, bg);
+        }
+        printf("\n");
+
+        patternIndex = 1 - patternIndex; // Alterna a ordem das cores de fundo a cada linha
+    }
+
+    printf(NEUTRAL "     A    B    C    D    E    F    G    H\n" RESET);
+}
+
+Status deducePositionFromRaw(char rawX, char rawY, int* column, int* row) {
+    switch (rawX) {
+        case 'a': *column = 0; break;
+        case 'b': *column = 1; break;
+        case 'c': *column = 2; break;
+        case 'd': *column = 3; break;
+        case 'e': *column = 4; break;
+        case 'f': *column = 5; break;
+        case 'g': *column = 6; break;
+        case 'h': *column = 7; break;
+        default: return Err;
+    }
+    switch (rawY) {
+        case '1': *row = 7; break;
+        case '2': *row = 6; break;
+        case '3': *row = 5; break;
+        case '4': *row = 4; break;
+        case '5': *row = 3; break;
+        case '6': *row = 2; break;
+        case '7': *row = 1; break;
+        case '8': *row = 0; break;
+        default: return Err;
+    }
+
+    return Ok;
+}
+
+void movePiece(Slot board[][DIM], int originColumn, int originRow, int destinationColumn, int destinationRow, Color color) {
+    // Deve existir estes índices dentro de board, senão comportamento indefinido
+
+    Slot piece = board[originRow][originColumn];
+    
+    if (color == White) {
+        board[originRow][originColumn] = WhiteTrail;
+    } else {
+        board[originRow][originColumn] = BlackTrail;
+    }
+
+    board[destinationRow][destinationColumn] = piece;
+}
+
+void clearPosition(Slot board[][DIM], int column, int row) {
+    // Devem existir estes índices dentro de board, senão comportamento indefinido
+    
+    board[row][column] = Empty;
+}
 
 int main() {
-    PIECE board[DIM * DIM] = {
-        PT1, PC1, PB1, PD1, PR1, PB2, PC2, PT2,
-        PP1, PP2, PP3, PP4, PP5, PP6, PP7, PP8,
-         X ,  X ,  X ,  X ,  X ,  X ,  X ,  X ,
-         X ,  X ,  X ,  X ,  X ,  X ,  X ,  X ,
-         X ,  X ,  X ,  X ,  X ,  X ,  X ,  X ,
-         X ,  X ,  X ,  X ,  X ,  X ,  X ,  X ,
-        BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8,
-        BT1, BC1, BB1, BD1, BR1, BB2, BC2, BT2,
+    Slot board[][DIM] = {
+        {Pt1, Pc1, Pb1, Pd1, Pr1, Pb2, Pc2, Pt2},
+        {Pp1, Pp2, Pp3, Pp4, Pp5, Pp6, Pp7, Pp8},
+        {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty},
+        {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty},
+        {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty},
+        {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty},
+        {Bp1, Bp2, Bp3, Bp4, Bp5, Bp6, Bp7, Bp8},
+        {Bt1, Bc1, Bb1, Bd1, Br1, Bb2, Bc2, Bt2},
     };
     
     printf(NEUTRAL "\n              INÍCIO DA PARTIDA\n\n" RESET);
-    displayBoard(board);
+    printBoard(board);
     printf("\n");
 
-    const char fromSequence[7][2] = {"e2", "e7", "f1", "b8", "d1", "g8", "h5"};
-    const char toSequence[7][2] = {"e4", "e5", "c4", "c6", "h5", "f6", "f7"};
-    const char* labels[7] = {
-        "Brancas jogam e4",
-        "Pretas jogam e5",
-        "Brancas jogam Bc4",
-        "Pretas jogam Cc6",
-        "Brancas jogam Dh5",
-        "Pretas jogam Cf6",
-        "Brancas capturam Peão, Xeque Mate: Dxf7#"
+    typedef struct {
+        int rawOriginX;
+        int rawOriginY;
+        int rawDestinationX;
+        int rawDestinationY;
+        char* label;
+    } Step;
+
+    const Step steps[] = {
+        {'e', '2', 'e', '4', "Brancas jogam e4"},
+        {'e', '7', 'e', '5', "Pretas jogam e5"},
+        {'f', '1', 'c', '4', "Brancas jogam Bc4"},
+        {'b', '8', 'c', '6', "Pretas jogam Cc6"},
+        {'d', '1', 'h', '5', "Brancas jogam Dh5"},
+        {'g', '8', 'f', '6', "Pretas jogam Cf6"},
+        {'h', '5', 'f', '7', "Brancas capturam Peão, xeque Mate: Dxf7#"},
     };
 
-    // Como os rastros são limpos apenas após a jogada de ambos
-    // é necessário guardar o índice do rastro da peça branca
-    int formerWhiteFrom;
+    for (int i = 0; i < sizeof(steps) / sizeof(Step); i++) {
+        int originColumn, originRow, destinationColumn, destinationRow;
 
-    for (int i = 0; i < 7; i++) {
-        ResultInt from = convertCoordinatesToIndex(fromSequence[i]);
-        ResultInt to = convertCoordinatesToIndex(toSequence[i]);
-        if (i % 2 == 0) {
-            formerWhiteFrom = from.value;
-            whiteMoveTo(board, from.value, to.value);
-            printf(
-                NEUTRAL "\n                  JOGADA #%d\n\n   %s\n" RESET,
-                i / 2 + 1,
-                labels[i]
-            );
-            displayBoard(board);
-            printf("\n");
-        } else {
-            blackMoveTo(board, from.value, to.value);
-            printf(NEUTRAL "   %s\n" RESET, labels[i]);
-            displayBoard(board);
-            printf("\n");
-
-            clearPosition(board, formerWhiteFrom);
-            clearPosition(board, from.value);
+        if (deducePositionFromRaw(steps[i].rawOriginX, steps[i].rawOriginY, &originColumn, &originRow) == Err) {
+            printf("Erro ao deduzir posição de origem.\n");
+            return 1;
         }
+
+        if (deducePositionFromRaw(steps[i].rawDestinationX, steps[i].rawDestinationY, &destinationColumn, &destinationRow) == Err) {
+            printf("Erro ao deduzir posição de destino.\n");
+            return 1;
+        }
+
+        printf(NEUTRAL);
+        if (i % 2 == 0) {
+            printf("\n                  JOGADA #%d\n\n", i / 2 + 1);
+            movePiece(board, originColumn, originRow, destinationColumn, destinationRow, White);
+        } else {
+            movePiece(board, originColumn, originRow, destinationColumn, destinationRow, Black);
+        }
+        printf("   %s\n" RESET, steps[i].label);
+        printBoard(board);
+        printf("\n");
+
+        clearPosition(board, originColumn, originRow);
     }
 
     return 0;
 }
 
-void displayBoard(PIECE* board) {
-    char* bg_1;
-    char* bg_2;
-
-    for (int i = 0; i < DIM; i++) {
-        // Inverte a ordem das cores de fundo a cada linha
-        if (i % 2 == 0) {
-            bg_1 = BACKGROUND_LIGHT;
-            bg_2 = BACKGROUND_DARK;
-        } else {
-            bg_1 = BACKGROUND_DARK;
-            bg_2 = BACKGROUND_LIGHT;
-        }
-    
-        printf(NEUTRAL " %d " RESET, 8 - i);
-        for (int j = 0; j < DIM; j++) {
-            char* bg = (j % 2 == 0) ? bg_1 : bg_2; // Alterna a cor de fundo a cada espaço
-            printf("%s %s " RESET, bg, PIECE_DISPLAY_ANSI[board[i * DIM + j]]);
-        }
-        printf("\n   ");
-        for (int j = 0; j < DIM; j++) {
-            char* bg = (j % 2 == 0) ? bg_1 : bg_2; // Alterna a cor de fundo a cada espaço
-            printf("%s     " RESET, bg);
-        }
-        printf("\n");
-    }
-    printf(NEUTRAL "     A    B    C    D    E    F    G    H\n" RESET);
-}
-
-ResultInt convertCoordinatesToIndex(char* coord) {
-    int X, Y;
-    ResultInt result;
-
-    // coord deve haver ao menos dois caracteres, senão comporrtamento indefinido
-    switch (coord[0]) {
-        case 'a': X = 0; break;
-        case 'b': X = 1; break;
-        case 'c': X = 2; break;
-        case 'd': X = 3; break;
-        case 'e': X = 4; break;
-        case 'f': X = 5; break;
-        case 'g': X = 6; break;
-        case 'h': X = 7; break;
-        default:
-            result.status = ERR;
-            return result;
-    }
-    switch (coord[1]) {
-        case '1': Y = 7; break;
-        case '2': Y = 6; break;
-        case '3': Y = 5; break;
-        case '4': Y = 4; break;
-        case '5': Y = 3; break;
-        case '6': Y = 2; break;
-        case '7': Y = 1; break;
-        case '8': Y = 0; break;
-        default:
-            result.status = ERR;
-            return result;
-    }
-
-    result.value = Y * DIM + X;
-    result.status = OK;
-    return result;
-}
-
-void whiteMoveTo(PIECE* board, int from, int to) {
-    // Deve existir estes índices dentro de board, senão comportamento indefinido
-    PIECE piece = board[from];
-    board[from] = WHITE_TRAIL;
-    board[to] = piece;
-}
-
-void blackMoveTo(PIECE* board, int from, int to) {
-    // Deve existir estes índices dentro de board, senão comportamento indefinido
-    PIECE piece = board[from];
-    board[from] = BLACK_TRAIL;
-    board[to] = piece;
-}
-
-void clearPosition(PIECE* board, int index) {
-    // Deve existir este índice dentro de board, senão comportamento indefinido
-    board[index] = X;
-}
